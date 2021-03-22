92fc1b802b0789c10bac419d7b221f3132cea7a6
Oleksiy-ver1 <kizimov1979@gmail.com>
2021-03-22 11:57:17 +0200

    created a new Interface and replaced the extension with it

--- src/handler/ChessboardHandler.java
+++ src/handler/ChessboardHandler.java
@@ -3,17 +3,16 @@ package handler;
 import model.Chessboard;
 import service.ChessboardService;
 import utility.ConsoleUtils;
-import utility.RepeatBlock;
 
 import java.io.IOException;
 
-public class ChessboardHandler extends RepeatBlock {
+public class ChessboardHandler implements Handler{
     public void manualInput() throws IOException {
         System.out.println("Введите параметры шахмотной доски:");
         byte height = ConsoleUtils.getPositiveByteMinValue("Введите высоту доски:", (byte) 2);
         byte width = ConsoleUtils.getPositiveByteMinValue("Введите ширину доски:", (byte) 2);
         String cell = ConsoleUtils.getSingleCharString("Введите значение для одиночный символ для заполнения:");
         System.out.print(new ChessboardService(new Chessboard(height, width, cell)).build());
-        if (ChessboardHandler.restart()) manualInput();
+        if (ConsoleUtils.restart("\nЖелаете повторить? (Yes/No):")) manualInput();
     }
 }
