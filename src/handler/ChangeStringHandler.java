457fd60b446254026d70e6a8bbbfc0a28747e396
Oleksiy-ver1 <kizimov1979@gmail.com>
2021-03-22 11:50:55 +0200

    renamed the class

--- src/handler/ChangeStringHandler.java
+++ src/handler/ChangeStringHandler.java
@@ -1,13 +1,12 @@
 package handler;
 
-import service.ChangeStringService;
-import utility.CheckFileVoidUtils;
+import utility.FileUtils;
 
 import java.io.IOException;
 import java.util.Scanner;
 
-public class ChangeStringHandler{
-    public static void manualInput() throws IOException {
+public class ChangeStringHandler implements Handler {
+    public void manualInput() throws IOException {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Укажите путь к файлу: ");
         String path2 = scanner.next();
@@ -15,8 +14,8 @@ public class ChangeStringHandler{
         String findString = scanner.next();
         System.out.print("Введите строку для замены: ");
         String replacementString = scanner.next();
-        if (CheckFileVoidUtils.validationFile(path2)) {
-            new ChangeStringService().parsFile(path2, findString, replacementString);
-        } else ChangeStringHandler.manualInput();
+        if (FileUtils.validationFile(path2)) {
+            new FileUtils().parsFile(path2, findString, replacementString);
+        } else manualInput();
     }
 }
