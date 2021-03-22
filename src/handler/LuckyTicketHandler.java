72b36c2edc44e17ac4c00c12fa34792fe6a9c488
Oleksiy-ver1 <kizimov1979@gmail.com>
2021-03-22 12:10:58 +0200

    created a new Interface and replaced the extension with it

--- src/handler/LuckyTicketHandler.java
+++ src/handler/LuckyTicketHandler.java
@@ -1,18 +1,17 @@
 package handler;
 
-import service.LuckyTicketService;
-import utility.CheckFileVoidUtils;
+import service.LuckyTicketServiceImplementation;
 import utility.ConsoleUtils;
-import utility.LuckyTicketUtils;
-import utility.RepeatBlock;
+import utility.FileUtils;
 
 import java.io.IOException;
 import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
 
-public class LuckyTicketHandler extends RepeatBlock {
+public class LuckyTicketHandler implements Handler {
     public void manualInput() throws IOException {
+        boolean result = true;
         final String MOSCOW_TYPE = "mosсow";
         final String PITER_TYPE = "piter";
         String message = "Введите номер билета (6 цыфр):";
@@ -21,21 +20,23 @@ public class LuckyTicketHandler extends RepeatBlock {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Укажите путь к файлу: ");
         String path1 = scanner.next();
-        if (CheckFileVoidUtils.validationFile(path1)) {
+        if (FileUtils.validationFile(path1)) {
             List<String> list = ConsoleUtils.parsFile(path1);
             String methodName = list.get(0).toLowerCase(Locale.ROOT);
             System.out.println(methodName);
             switch (methodName) {
                 case MOSCOW_TYPE:
-                    new LuckyTicketService().methodMoscow(new LuckyTicketUtils().getUnit(fromConsole));
+                    result = new LuckyTicketServiceImplementation().methodMoscow(new LuckyTicketServiceImplementation().getUnit(fromConsole));
                     break;
                 case PITER_TYPE:
-                    new LuckyTicketService().methodPiter(new LuckyTicketUtils().getUnit(fromConsole));
+                    result = new LuckyTicketServiceImplementation().methodPiter(new LuckyTicketServiceImplementation().getUnit(fromConsole));
                     break;
             }
-        } else new LuckyTicketHandler().manualInput();
+        } else manualInput();
+        if (result) System.out.println("Счастливый билетик!");
+        if (!result) System.out.println("Не счастливый билет");
 
-        if (LuckyTicketHandler.restart()) manualInput();
+        if (ConsoleUtils.restart("\nЖелаете повторить? (Yes/No):")) manualInput();
 
     }
 
