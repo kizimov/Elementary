7ef9b4fcd9373761ad2fcddda04bbe0c433b409b
Oleksiy-ver1 <kizimov1979@gmail.com>
2021-03-22 12:05:13 +0200

    implements Handler renamed method CheckFileVoidUtils ->FileUtils CountEntryHandler ->manualInput

--- src/handler/CountEntryHandler.java
+++ src/handler/CountEntryHandler.java
@@ -1,20 +1,20 @@
 package handler;
 
 import service.CountEntryService;
-import utility.CheckFileVoidUtils;
+import utility.FileUtils;
 
 import java.io.IOException;
 import java.util.Scanner;
 
-public class CountEntryHandler{
-    public static void manualInput() throws IOException {
+public class CountEntryHandler implements Handler{
+    public void manualInput() throws IOException {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Укажите путь к файлу: ");
         String path1 = scanner.next();
         System.out.println("Введите образец для подсчета: ");
         String sample = scanner.next();
-        if (CheckFileVoidUtils.validationFile(path1)) {
+        if (FileUtils.validationFile(path1)) {
             new CountEntryService().parsFile(path1, sample);
-        } else CountEntryHandler.manualInput();
+        } else manualInput();
     }
 }
